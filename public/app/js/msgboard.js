1b369082be734a85febb60b10e77ef7788afe366
hainesnoids <127229@lrstudents.org>
2026-01-09 11:17:58 -0500

    message board page 222222224

---
+++ public/app/js/msgboard.js
@@ -0,0 +1,40 @@
+let windowScale;
+
+document.addEventListener('DOMContentLoaded', function() {
+    const main = document.querySelector('#main'),
+        mainHeight = main.getBoundingClientRect().height,
+        mainWidth = main.getBoundingClientRect().width,
+        mainAspect = 16/9;
+    let resizeTimer;
+
+    // Calls rescale when window resizes
+    window.addEventListener('resize', function(e) {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(scaleWindow, 100);
+    });
+
+    function scaleWindow() {
+        let scale, windowAspect;
+
+        windowAspect = window.innerWidth / window.innerHeight;
+
+        if (windowAspect >= mainAspect) {
+            scale = window.innerHeight / mainHeight;
+        } else {
+            scale = window.innerWidth / mainWidth;
+        }
+
+        main.style.transform = `translate(-50%, -50%) scale(${scale})`;
+        windowScale = scale;
+    }
+
+    scaleWindow(); // init
+    document.querySelector(".main").addEventListener("click",fullscreen);
+});
+
+function fullscreen() {
+    const main = document.body.querySelector('main');
+    main.requestFullscreen({navigationUI: "hide"}).then();
+}
+
+homePageAutoUpdate().then();
